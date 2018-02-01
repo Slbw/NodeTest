555deb7eba0c151e81b068c523653581e95056da
Slbw <teng1234567ha@126.com>
2018-02-01 15:28:18 +0800

    测试更新

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require("http");
  
 http.createServer(function(req,res){
   res.writeHead(200,{"Content-Type":"text/html"});
-  res.write("<h1>Nodejs demo one</h1>");
-  res.end("<p>Hello World2</p>");
+  res.write("<h1>heiheihei</h1>");
+  res.end("<p>good</p>");
 }).listen(80);
 console.log("Http server is listening at port 80");
